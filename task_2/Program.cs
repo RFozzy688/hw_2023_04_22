dc3af734824f9fc4ea1d2c09175b94beecb49c2b
RFozzy688 <pokerfozzy@gmail.com>
2023-04-26 23:50:05 +0300

    creating and testing delegate CalcDelegate

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -13,10 +13,66 @@ using System.Threading.Tasks;
 
 namespace task_2
 {
+    public delegate double CalcDelegate(double x, double y);
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public double Mult(double x, double y)
+        {
+            return x * y;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Calculator calc = new Calculator();
+
+            Dictionary<char, CalcDelegate> actions = new Dictionary<char, CalcDelegate>();
+            actions['+'] = calc.Add;
+            actions['-'] = calc.Sub;
+            actions['*'] = calc.Mult;
+
+            Console.Write("Enter an expression: ");
+            string expression = Console.ReadLine();
+
+            char sign = ' ';
+
+            // определения знака арифметического действия
+            foreach (char item in expression)
+            {
+                if (item == '+' || item == '-' || item == '*')
+                {
+                    sign = item;
+                    break;
+                }
+            }
+
+            try
+            {
+                if (sign != ' ')
+                {
+                    string[] numbers = expression.Split(sign);
+
+                    Console.WriteLine($"Result: {actions[sign](double.Parse(numbers[0]), double.Parse(numbers[1]))}");
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }

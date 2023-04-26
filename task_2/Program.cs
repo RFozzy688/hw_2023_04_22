598acd44dab3d18fbba096a01a08dd4d75f2cf1c
RFozzy688 <pokerfozzy@gmail.com>
2023-04-26 23:14:47 +0300

    added task discription

---
+++ task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Завдання 2
+//Створіть додаток для виконання арифметичних операцій. Підтримувані операції:
+// ■ Додавання двох чисел;
+// ■ Віднімання двох чисел;
+// ■ Множення двох чисел.
+//Код програми обов’язково має використати механізм делегатів.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

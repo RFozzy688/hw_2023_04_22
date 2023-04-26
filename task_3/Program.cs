9fbd8344b280f55c2ad002b881e5219624d4f704
RFozzy688 <pokerfozzy@gmail.com>
2023-04-26 23:59:33 +0300

    added task discription

---
+++ task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Завдання 3
+//Створіть додатки для виконання арифметичних операцій. Підтримувані операції:
+// ■ Перевірка числа на парність;
+// ■ Перевірка числа на непарність;
+// ■ Перевірка на просте число;
+// ■ Перевірка на число Фібоначчі.
+//Обов’язково використовуйте делегат типу Predicate.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
